7f22ec027697ae7fb7121d1a9475e4cb822dc56f
Sovi11 <pravarkataria@gmail.com>
2024-01-08 18:22:46 +0530

    Adding files

---
+++ demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    // printf("Hello world\n") ;
+    // int input ;
+    // scanf("%d",&input) ;
+    // if (input > 12) printf("Nice\n") ;
+    // else printf("Not nice\n") ;
+    // printf("Hi this is %d\n" , input) ;
+}
